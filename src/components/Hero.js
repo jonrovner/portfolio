014c10dba2b4e1b14ea577bde5abcd23bf1908e1
jonrovner <jonrovner@gmail.com>
2022-04-09 19:36:49 -0300

    developing home component

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -5,16 +5,39 @@ import { typeSentence} from '../utils.js'
 
 const Hero = () => {
     useEffect( ()=>{
-        setTimeout(()=>{typeSentence("Full-Stack Web developer", "title", 150)}, 2200)
-
-    
+        setTimeout(()=>{typeSentence("Full-Stack Web developer", "title", 50)}, 2200)
+        setTimeout(()=>{typeSentence("I create responsive front-ends, realiable APIs and Databases", "subtitle", 10)}, 3500)
     }, [])
     
     return (
+        <div className='home'>
+        
         <section id="hero">
+
             <div id="title"></div>
-          
+            <div id="subtitle"></div>       
+       
         </section> 
+        <section id="features">
+            <div className='feature' id='responsive'>
+                <img src='responsiveIcon.png' alt="responsive icon"/>
+                <h3>Front-end development</h3>
+                <p>Responsive websites and SPAs with optimal user experience</p>
+            </div>
+            <div className='feature' id='server'>
+                <img src='server.png' alt="server icon"/>
+                <h3>Servers and APIs</h3>
+                <p>Reliable back-end developed for scalability and performnance</p>
+
+            </div>
+            <div className='feature' id='database'>
+            <img src='database.png' alt="database icon"/>
+                <h3>Database development</h3>
+                <p>Well structured efficient databases for data persistence</p>
+
+            </div>
+        </section> 
+        </div>
     );
 }
 
