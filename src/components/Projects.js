13fb538bcdce37cd03a4ae5699d64e101841e496
jonrovner <jonrovner@gmail.com>
2022-04-13 15:52:13 -0300

    improved layout and data from json

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,85 +1,71 @@
 import React from 'react';
+import projects from './data';
 
 const Projects = () => {
     return (
+        <>
         <div className='projects'>
-            <h2>Featured Projects</h2>
+            <h2>Featured Project</h2>
             <div className='project'>
 
                 <div className='projectHead'>
                     <div className='title'>
-                        <img src='assets/canchera_logo.png' alt="logo"/>
+                        <img src={projects[0].logo} alt="logo"/>
                     </div>
                     <div className='description'>
-                        <p>Canchera is a Single Page Application developed as the final project for Henry Bootcamp. I´s a football field booking application. I designed and developed the booking calendar and serveral components with ReactJS, </p>
-
+                        <p>{projects[0].description}</p>
                     </div>
-
                 </div>
                 <div className='projectImage'>
-                    <img src='assets/canchera1.png' alt='canchera'/>
-
+                    <img src={projects[0].image} alt='canchera'/>
                 </div>
                 <div className='links'>
-                    <p><a href='https://canchera.vercel.app/' target='_blank' rel="noreferrer">demo</a> </p>
-                    <p><a href='https://github.com/jonrovner/Canchera' target='_blank' rel="noreferrer">code</a></p>
-
+                    <p><a href={projects[0].deploy} target='_blank' rel="noreferrer">deploy</a> </p>
+                    <p><a href={projects[0].code} target='_blank' rel="noreferrer">code</a></p>
                 </div>
 
 
 
             </div>
-            <div className='project'>
+        </div>      
+        <div className='otherProjects'>
 
+            <div className='project'>
                 <div className='projectHead'>
                     <div className='title'>
-                        <img src='assets/countries_logo.png' alt="logo"/>
+                        <img src={projects[1].logo} alt="logo"/>
                     </div>
                     <div className='description'>
-                        <p>Countries is a Single Page Application developed as an individual project for Henry Bootcamp. It has filtered search, controlled forms, and back-end with API and database</p>
-
+                        <p>{projects[1].description}</p>
                     </div>
-
                 </div>
-                <div className='projectImage'>
-                    <img src='assets/countries1.png' alt='canchera'/>
 
-                </div>
                 <div className='links'>
-                    <p><a href='https://canchera.vercel.app/' target='_blank' rel="noreferrer">demo</a> </p>
-                    <p><a href='https://github.com/jonrovner/Canchera' target='_blank' rel="noreferrer">code</a></p>
-
+                    <p><a href={projects[1].deploy} target='_blank' rel="noreferrer">demo</a> </p>
+                    <p><a href={projects[1].code} target='_blank' rel="noreferrer">code</a></p>
                 </div>
-
-
-
             </div>
+            
             <div className='project'>
-
                 <div className='projectHead'>
                     <div className='title'>
-                        <img src='assets/antonia_logo.png' alt="logo"/>
+                        <img src={projects[2].logo} alt="logo"/>
                     </div>
                     <div className='description'>
-                        <p>Muy Antoniadis is a Single Page Application developed as the final project for Henry Bootcamp. I´s a football field booking application. I designed and developed the booking calendar and serveral components with ReactJS, </p>
-
+                    <p>{projects[2].description}</p>
                     </div>
-
-                </div>
-                <div className='projectImage'>
-                    <img src='assets/antonia1.png' alt='canchera'/>
-
                 </div>
                 <div className='links'>
-                    <p><a href='https://canchera.vercel.app/' target='_blank' rel="noreferrer">demo</a> </p>
-                    <p><a href='https://github.com/jonrovner/Canchera' target='_blank' rel="noreferrer">code</a></p>
-
+                    <p><a href={projects[2].deploy} target='_blank' rel="noreferrer">demo</a> </p>
+                    <p><a href={projects[2].code} target='_blank' rel="noreferrer">code</a></p>
                 </div>
-
-
-
             </div>
+
         </div>
+
+
+         </>   
+       
     );
 }
 
---
+++ src/components/data.js
@@ -0,0 +1,24 @@
+const projects = [
+    {
+    logo: 'assets/canchera_logo.png',
+    description: 'Canchera is a Single Page Application developed as the final project for Henry Bootcamp. I´s a football field booking application. I designed and developed the booking calendar and serveral components with ReactJS, Redux and CSS',
+    image: 'assets/canchera1.png',
+    deploy: 'https://canchera.vercel.app',
+    code: 'https://github.com/jonrovner/Canchera'
+    },
+    {
+    logo: 'assets/countries_logo.png',
+    description: 'Countries is a Single Page Application developed as an individual project for Henry Bootcamp. It has filtered search, controlled forms, and back-end with API and database',
+    image: 'assets/countries1.png',
+    deploy: 'https://rocky-scrubland-68336.herokuapp.com',
+    code: 'https://github.com/jonrovner/picountries'
+    },
+    {
+    logo: 'assets/antonia_logo.png',
+    description: 'Muy Antoniadis is a Single Page Ecommerce Application developed with ReactJS, Bootstrap, Context API, Express server and Paypal checkout integration',
+    image: 'assets/antonia1.png',
+    deploy: 'https://conservative-eh-60074.herokuapp.com/',
+    code: 'https://github.com/jonrovner/muyantoniadis'
+    },
+]
+export default projects
